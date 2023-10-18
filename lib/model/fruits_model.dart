19bab3189e792a8da28acd2a91af4f7004724129
Tech Asif <asif.letsgobd@gmail.com>
2023-10-18 13:29:06 +0600

    ...

--- lib/model/fruits_model.dart
+++ lib/model/fruits_model.dart
@@ -4,8 +4,9 @@ class FruitsModel {
   String name ;
   int qty;
   double price;
+  String type;
 
-  FruitsModel(this.id,this.img,this.name,this.qty,this.price);
+  FruitsModel(this.id,this.img,this.name,this.qty,this.price, this.type);
 
   // static List<Fruits> fruits_details(){
   //   return [
@@ -26,12 +27,12 @@ class FruitsModel {
 
  List<FruitsModel> fruitList =
  [
-FruitsModel(1,'images/strawberry.png','Strawberry',75,10.14),
-FruitsModel(2,'images/strawberry2.jpeg','Strawberry',57,14.10),
-FruitsModel(3,'images/apple.jpg','Apple',75,10.14),
-FruitsModel(4,'images/orange.png','Orange',44,10.14),
-FruitsModel(5,'images/dragon.jpg','Dragon',60,11.14),
-FruitsModel(6,'images/capsicum.jpg','Capsicum',70,10.14),
-FruitsModel(7,'images/orange2.jpeg','Orange2',16,78.14),
-FruitsModel(8,'images/dragon2.png','Dragon2',18,67.14)
+FruitsModel(1,'images/strawberry.png','Strawberry',75,10.14,"pending"),
+FruitsModel(2,'images/strawberry2.jpeg','Strawberry',57,14.10,"pending"),
+FruitsModel(3,'images/apple.jpg','Apple',75,10.14,"pending"),
+FruitsModel(4,'images/orange.png','Orange',44,10.14,"processing"),
+FruitsModel(5,'images/dragon.jpg','Dragon',60,11.14,"processing"),
+FruitsModel(6,'images/capsicum.jpg','Capsicum',70,10.14,"processing"),
+FruitsModel(7,'images/orange2.jpeg','Orange2',16,78.14,"delivered"),
+FruitsModel(8,'images/dragon2.png','Dragon2',18,67.14,"delivered")
 ];
--- lib/pages/order.dart
+++ lib/pages/order.dart
@@ -1,3 +1,4 @@
+import 'package:ecom/model/fruits_model.dart';
 import 'package:flutter/material.dart';
 
 class Order extends StatefulWidget {
@@ -8,11 +9,177 @@ class Order extends StatefulWidget {
 }
 
 class _OrderState extends State<Order> {
+
+  final List _items = [
+    'All',
+    'Pending',
+    'Processing',
+    'Delivered'
+  ];
+
+  var _isSelected = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.greenAccent,
-      body: Center(child: Text("Order"),),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.deepPurple,
+        body: Container(
+          padding: const EdgeInsets.all(10),
+          decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              )),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Daily\nGrocery Food",
+                    style: TextStyle(fontSize: 34, color: Colors.black),
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    height: 80,
+                    width: 60,
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(80),
+                    ),
+                    child: const Icon(
+                      Icons.search_rounded,
+                      size: 35,
+                    ),
+                  )
+                ],
+              ),
+              Container(
+                height: 60,
+                child: ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: _items.length,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _isSelected = index;
+                          fruitList.where((element) => element.type == _items[_isSelected]).toList();
+                        });
+                      },
+                      child: Container(
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                            color: _isSelected == index
+                                ? Colors.black
+                                : Colors.blue,
+                            borderRadius: BorderRadius.circular(
+                              40,
+                            )),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Text(
+                            _items[index],
+                            style: TextStyle(
+                                fontSize: 16,
+                                color: _isSelected == index
+                                    ? Colors.white
+                                    : Colors.black),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  separatorBuilder: (context, index) {
+                    return const SizedBox(
+                      width: 10,
+                    );
+                  },
+                ),
+              ),
+
+              const SizedBox(height: 10,),
+
+              Expanded(
+                  child: ListView.separated(
+                    itemBuilder: (context, index) {
+                      return Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16),
+                          color: Colors.red,
+                        ),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.grey.shade200,
+                              borderRadius: BorderRadius.circular(16)
+                          ),
+                          padding: const EdgeInsets.all(10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Image(
+                                    image: AssetImage(fruitList[index].img),
+                                    width: 80,
+                                  ),
+                                  const SizedBox(
+                                    width: 20,
+                                  ),
+                                  Column(
+                                    children: [
+                                      Text(
+                                        fruitList[index].name,
+                                        style: const TextStyle(fontSize: 16),
+                                      ),
+                                      Text("${fruitList[index].qty}"),
+                                      Text(
+                                        "${fruitList[index].price}",
+                                        style: const TextStyle(fontSize: 16),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  const Text(
+                                    "+",
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                                  Container(
+                                      height: 20,
+                                      width: 20,
+                                      alignment: Alignment.center,
+                                      decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(3),
+                                          color: Colors.black),
+                                      child: const Text(
+                                        "1",
+                                        style: TextStyle(
+                                            fontSize: 14, color: Colors.white),
+                                      )),
+                                  const Text("-", style: TextStyle(fontSize: 20)),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return const SizedBox(
+                        height: 20,
+                      );
+                    },
+                    itemCount: fruitList.length,
+                  ))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
